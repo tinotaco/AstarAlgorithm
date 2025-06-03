6f11353262d947fad434aa1d7849b1a89ba7744f
tinotaco <constantin.supper2015@gmail.com>
2025-06-04 00:05:03 +0200

    endAnimation and create main file

--- astarmethod.py
+++ astarmethod.py
@@ -2,24 +2,9 @@ import numpy as np
 from environment import Environment
 import environment
 from environment import Point
-import matplotlib.pyplot as plt
-from matplotlib import colors
-import matplotlib.animation as animation
 import math
 
-# f = g + h
-
-# What information do i need in each element of the matrix?
-# g definitely
-# h maybe?
-# [0-8] value in which direction the parent is (0, is itself, 1, north, then counterclockwise)
-
-# Point is an array with 2 elements. The 0th is x, the 1st is y
-
-# Access to map array and environment is with matrix notation and not cartesian access. map[row, column] not map[x, y]
-
-#def parseDirection(point, parent
-
+global ani
 
 class MapInfo:
     def __init__(self):
@@ -56,9 +41,18 @@ class AStarMethod():
                 self.map[i, j] = MapInfo()
         self.start = environment.startpos
         self.end = environment.endpos
-        self.map[self.start.y, self.start.x].setInfo(g = 0, h = self.endHeuristic(self.start, self.end))
-        self.frontier_queue = [[self.start, self.map[self.start.y, self.start.x].getTotalHeuristic()]]
+        self.exec_point = []
+        self.past_exec_points = []
+        self.frontier_queue = []
     
+    def initExecution(self):
+        self.past_exec_points = []
+        self.frontier_queue = []
+        self.exec_point = self.start
+        self.map[self.start.y, self.start.x].setInfo(g = 0, h = self.endHeuristic(self.start, self.end))
+        self.addNewFrontierElements(self.start)
+        print(f"New FrontierElements after init are: {self.frontier_queue}")
+
     def endHeuristic(self, point, end): # h
         dx = abs(end.x - point.x)
         dy = abs(end.y - point.y)
@@ -81,6 +75,8 @@ class AStarMethod():
         pointTotalHeuristic = self.map[point.y, point.x].getTotalHeuristic()
         if len(self.frontier_queue) == 0:
             self.frontier_queue.insert(0, [point, pointTotalHeuristic])
+            self.map[point.y, point.x].queuePoint = True
+            return
         if self.map[point.y, point.x].queuePoint is True:
             for i in range(len(self.frontier_queue)):
                 elem = self.frontier_queue[i]
@@ -92,22 +88,14 @@ class AStarMethod():
                         break
         for i in range(len(self.frontier_queue)):
             if self.frontier_queue[i][1] >= self.map[point.y, point.x].getTotalHeuristic():
+                added_value = True
                 self.frontier_queue.insert(i, [point, self.map[point.y, point.x].getTotalHeuristic()])
                 self.map[point.y, point.x].queuePoint = True
-                break
-
-
-
-
-
-
-
-    #def get_shortest_path(self, start, end):
-        # Add frontier elements
-
-        # Execute Frontier
-
-        # Update Information
+                return
+        # Newest element contains largest value in frontier queue list therefore placed at back
+        self.frontier_queue.append([point, self.map[point.y, point.x].getTotalHeuristic()])
+        self.map[point.y, point.x].queuePoint = True
+        
     
     def addNewFrontierElements(self, point):
         for rel_row in range(-1, 2, 1): #list from -1 to 1 for relative row 
@@ -121,63 +109,28 @@ class AStarMethod():
                     self.updateFrontierQueuePoint(eval_point)
 
     def executeFrontierPoint(self):
+        self.past_exec_points.append(Point(self.exec_point.x, self.exec_point.y))
         execPoint = self.frontier_queue.pop(0)[0]
         self.exec_point = execPoint
+        if self.exec_point.x == self.end.x and self.exec_point.y == self.end.y:
+            return False # The end point has been reached, stop algorithm
         self.map[self.exec_point.y, self.exec_point.x].queuePoint = False
-        # print(f"Frontier is: \n{self.frontier_queue}")
-        # print(f"execPoint is: {execPoint}")
         self.addNewFrontierElements(execPoint)
-
-    def displayAlgorithm(self):
-        fig, ax = plt.subplots(figsize= (10,10))
-        cmap = colors.ListedColormap(['white', 'black'])
-
-        ax.imshow(self.env, cmap=cmap)
-        ax.grid(which='major', axis='both', linestyle='-', color='k', linewidth=1)
-
-        ax.set_xticks(np.arange(0.5, self.env.shape[1], 1))
-        ax.set_yticks(np.arange(0.5, self.env.shape[0], 1))
-        plt.tick_params(axis='both', which='both', bottom=False,   
-            left=False, labelbottom=False, labelleft=False)
-        
-        ax.scatter(self.start.x, self.start.y, marker='x', color = 'red', s=80)
-        ax.scatter(self.end.x, self.end.y, marker='*', color = 'red', s = 80)
-        ax.scatter(self.exec_point.x, self.exec_point.y, color='grey')
-        for elem in self.frontier_queue:
-            ax.scatter(elem[0].x, elem[0].y, marker='o', color = 'blue')
-        #ani = animation.FuncAnimation(fig=fig, func=self.executeFrontierPoint)
-        # Print environemnt, then all frontier points, and where i currently am
-        plt.show()
-        
-
-
-
-
-
-                    
-
-
-
+        return True
     
-    #def shortest_path_iteration(self, point):
-
-
-
-#Setup Environment
-env = Environment(50, 50)
-env.addblockrow(20, 35, 20)
-env.addblockrow(10, 10, 15)
-env.addblockcolumn(10, 20, 10)
-env.addblockcolumn(22, 5, 20)
-env.setstartpos(10, 5)
-env.endpos(30, 40)
-#env.plotenvironment()
-astar = AStarMethod(env)
-astar.executeFrontierPoint()
-astar.displayAlgorithm()
-#for i in range(0, 10):
-#    astar.executeFrontierPoint()
-#    astar.displayAlgorithm()
-#    print("\n\n\n")
+    def extractShortestPath(self):
+        if not self.map[self.end.y, self.end.x].containsValidInfo():
+            raise RuntimeError("Trying to get shortest path but end point has not been evaluated yet!")
+        x_path = [self.end.x]
+        y_path = [self.end.y]
+        current_point = self.end
+        while True:
+            new_parent = self.map[current_point.y, current_point.x].par
+            x_path.append(new_parent.x)
+            y_path.append(new_parent.y)
+            if new_parent.x == self.start.x and new_parent.y == self.start.y:
+                break;
+            current_point = new_parent
+        return x_path, y_path
 
 
